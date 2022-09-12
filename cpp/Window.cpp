a89ae7e6e760435c93a4a37f38e73ba2ff052d78
Jetvac2 <Jetvac2@icloud.com>
2022-09-11 20:26:57 -0400

    GLFW Window  set up!

---
+++ cpp/Window.cpp
@@ -0,0 +1,42 @@
+#include <GLFW/glfw3.h>
+
+#include "Window.h"
+
+namespace NWindow
+{
+    int Window::run(void)
+    {
+        GLFWwindow* window;
+
+        /* Initialize the library */
+        if (!glfwInit())
+            return -1;
+
+        /* Create a windowed mode window and its OpenGL context */
+        window = glfwCreateWindow(640, 480, "UpperEngine", NULL, NULL);
+        if (!window)
+        {
+            glfwTerminate();
+            return -1;
+        }
+
+        /* Make the window's context current */
+        glfwMakeContextCurrent(window);
+        glClearColor(1, 0, 0, 1);
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window))
+        {
+            /* Render here */
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+
+        glfwTerminate();
+        return 0;
+    }
+}
---
+++ includes/Window.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+
+namespace NWindow
+{
+	class Window
+	{
+	public:
+		int run();
+	};
+}
+
+
+
+
